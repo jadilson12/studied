754d7aa49121e3db8656a6d14a96a16baf3d12ac
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:13:15 -0300

    adiciona:  " condição for " como calcular nome de várias pessoas

---
+++ 54 - Ano de nascimento de varias pessoal com for.py
@@ -0,0 +1,14 @@
+from datetime import date
+maior = 0
+menor = 0
+atual = date.today().year
+for c in range(1,8):
+    anos = int(input('{}º Data de nascimento: '.format(c)))
+    idade = atual - anos
+    if idade < 21:
+        menor += 1
+    else:
+        maior += 1
+print('''  Resultado
+ Maiores de idade foram {} 
+ Menores de idade foram {}'''.format(maior,menor))
