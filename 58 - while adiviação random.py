0839d526e6066ec5c3ab19ddddff21ddc5b5ecbd
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:16:10 -0300

    adiciona:  " condição while "  exemplo com adiação random

---
+++ 58 - while adiviação random.py
@@ -0,0 +1,18 @@
+from random import randint
+pc = randint(0, 10)
+print(pc)
+print('Sou seu computador acabei de pensar um numero entre 0 a 10, ')
+print('Será que você consegue adiviar? ')
+acertou = False
+palpites = 0
+while not acertou:
+    palpites += 1
+    jogador = int(input('Qual é seu palpite ?  '))
+    if jogador == pc:
+        acertou = True
+    else:
+        if jogador < pc:
+                print('Mais ... Tente mais uma vez. ')
+        elif jogador > pc:
+                print('Menos...Tente mais uma vex. ')
+print('Para acerta você tentou {} vezes! parabés  '.format(palpites))
