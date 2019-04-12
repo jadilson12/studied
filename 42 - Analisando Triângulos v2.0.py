3c68af40806f2d12d2817dd450e5e4a85f2a0dc9
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:01:57 -0300

    adiciona:  if e else analizando um triangulo

---
+++ 42 - Analisando Triângulos v2.0.py
@@ -0,0 +1,14 @@
+a = float(input('Primeiro valor: '))
+b = float(input('Sengundo valor: '))
+c = float(input('Teceiro valor: '))
+if a < c + b and b < a + c and c < b + a:
+    print('Pode formar um triagulo ', end='')
+    if a == b == c:
+        print('EQUILÁTERO')
+    elif a != b != c != a:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print("Não pode formar um triagulo")
+print('-'*30)
