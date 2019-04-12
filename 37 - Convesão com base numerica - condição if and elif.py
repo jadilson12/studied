9398a0e9ffdfb14d06060eb4ac5587d9b9ba4de0
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:59:33 -0300

    adiciona:  if e else convesão com base numerica

---
+++ 37 - Convesão com base numerica - condição if and elif.py
@@ -0,0 +1,14 @@
+#Escreva um programa que leia um numero interiro qualquer e depois peça para comveter para base binaria
+n = int(input("Digite um numero inteiro : "))
+print('='*20)
+print("{}\n{}\n{}".format("1 - Binário","2 - Octal","3 - Hexadecimal"))
+print('='*20)
+opcao = int(input('Digite opção:'))
+if opcao == 1:
+    print('{} convertido para Binário foi a {}'.format(n,bin(n)[2:]))
+elif opcao == 2:
+    print('{} convertido para Octal foi a {}'.format(n, oct(n)[2:]))
+elif opcao == 3:
+    print('{} convertido para Hexadecimal foi a {}'.format(n, hex(n)[2:]))
+else:
+    print("Opçao invalida")
