a2924a330048b61340ffa0c17067d93a2ece5b0c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:14:22 -0300

    adiciona:  " concdição for " como sexo e idade

---
+++ 56 - Nome - sexo - idade com for.py
@@ -0,0 +1,17 @@
+a = 0
+b = 0
+c = 0
+n = ''
+for i in range(4):
+    nome = str(input('Digite o nome :')).strip()
+    idade = int(input('olá {} digite sua idade  '.format(nome)))
+    sexo = str(input('Digite M para masculino e F para feminino  ')).upper().strip()
+    a += idade
+    if sexo == 'M' and idade > b:
+            b = idade
+            n = nome
+    if sexo == 'F' and idade < 20:
+        c += 1
+print('\nA média de idade do grupo é: {}'.format(a/4))
+print('O homem mais velho é: {}'.format(n))
+print('{} mulheres tem menos de 20 anos'.format(c))
