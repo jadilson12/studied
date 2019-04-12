b9829607b012bd46b8b9a35aead060facf5b073d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:51:36 -0300

    adiciona: como mostar os digito de um numero

---
+++ 23 - Mostar os digitos de numeros.py
@@ -0,0 +1,5 @@
+n = input("Digite um mumero entre 0 a 9999: ")
+print('Unidade:{}'.format(n[3:4]))
+print('Dezena:{}'.format(n[2:3]))
+print('Centena:{}'.format(n[1:2]))
+print('Milhar:{}'.format(n[0:1]))
