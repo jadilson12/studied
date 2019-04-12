9e30821cb4a5ff97972b0d6910735929ccbbb0fa
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:01:29 -0300

    adiciona:  if e else criação de uma tabela de natação

---
+++ 41 - Tabela de natação.py
@@ -0,0 +1,15 @@
+from datetime import date
+ano_atual = date.today().year
+ano_nasceu = int(input('Data de nascimento: '))
+idade= ano_atual - ano_nasceu
+print('A idade é {} anos'.format(idade))
+if idade < 9:
+    print('O atleta é MIRIM')
+elif 9 <= idade < 14:
+    print('O atleta é INFANTIL')
+elif 14 <= idade < 19:
+    print('O atleta é JUNIOR')
+elif 19 <= idade < 25:
+    print('O atleta é SENIOR')
+else:
+    print('O atleta é MASTER')
