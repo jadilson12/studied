f9b9f71a58db87ea27381f56089e1ec4a80de54c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:07:56 -0300

    adiciona:  " condição for "  soma de valores pares

---
+++ 50 - Soma valores pares com for.py
@@ -0,0 +1,10 @@
+soma = 0
+cont = 0
+for c in range(0,7):
+        n = int(input("Digite um {} valor:".format(c)))
+        if n % 2 ==0:
+                soma += n
+                cont += 1
+
+
+print('A soma de Pares foi {} A soma dos némeros pares foram {}'.format(cont, soma))
