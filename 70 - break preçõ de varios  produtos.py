595022902feb632c7daa6607ba5bb6be48c612d9
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:21:46 -0300

    adiciona:  " break "  preço de vario produtos

---
+++ 70 - break preçõ de varios  produtos.py
@@ -0,0 +1,22 @@
+soma = total = cont = menor = totmil = 0
+barato = ' '
+while True:
+    produto = str(input('Nome produto: '))
+    preco = float(input('Preço R$'))
+    pregunta = ' '
+    while pregunta not in 'SN':
+        pregunta = str(input('Quer continuar [S/n]')).strip().upper()[0]
+    total += preco
+    cont += 1
+    if preco > 1000:
+        totmil += 1
+    if cont == 1  or preco < menor:
+        menor = preco
+        barato = produto
+    if pregunta == 'N':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'Total de gasto foi {total}')
+print(f'Temos {totmil} que custam mais de 1000 reais')
+print(f'O {barato} é o mais barato')
+
