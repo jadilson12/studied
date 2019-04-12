21adbdbf97d79a1605a8ac3c8903f684af1e61f7
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:38:37 -0300

    adiciona: exibir drobo, triplo e raiz quardrada

---
+++ 06 - Exibi o drobo e triplo e rais quandrada.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero inteiro: '))
+d = n1 * 2
+t = n1 * 3
+r = n1**(1/2)
+print('O doblo de {} é {} \nO triplo de {} é {} \nA raiz quandrada de {} é {:.2f}'.format(n1, d, n1, t, n1, r))
