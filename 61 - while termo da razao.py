abf86383926301f005df8fbd9914b120e6cc10cd
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:17:18 -0300

    adiciona:  " condição while " termo da razão PA II

---
+++ 61 - while termo da razao.py
@@ -0,0 +1,12 @@
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Digite o 1ª termo de uma PA: '))
+razao = int(input('Digite a razão dessa PA: '))
+termo = primeiro
+cont = 1
+while cont <=10:
+        print('{}'.format(termo), end = ' ► ')
+        termo +=razao
+        cont += 1
+print('Acabou')
+
