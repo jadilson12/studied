716b41d7b5c02bb86c76acd8c703a7a137938255
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:57:11 -0300

    adiciona:  if  como calcular a diferencia de salário

---
+++ 34 - Calcular a diferencia de um sálario com almento.py
@@ -0,0 +1,8 @@
+salario = int(input('Digite o sálario de um fucionário: '))
+if salario > 1250:
+    aumento = ((salario * 10) / 100)
+if salario <= 1250:
+    aumento = ((salario * 15) / 100)
+print('O valor que vai receber a mais será R$ {} reais'.format(aumento))
+print('Então o novo sário será de R$ {} reais'.format(aumento + salario))
+print('-----FIM----')
