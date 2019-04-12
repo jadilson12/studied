62534425f40e61edbdbf69c76f61a891ed670296
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:20:24 -0300

    adiciona:  " break "  usando na tabuada

---
+++ 67 - break tabuada varios numeros.py
@@ -0,0 +1,16 @@
+print('-='*20)
+print('-='*6, 'T A B U A D A','-='*6)
+print('-='*20)
+t = 0
+v = 1
+while True:
+    n = int(input('Qual o numero que você que ver na tabuada? '))
+    print('-=' * 20)
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n*c}')
+    print('-=' * 20)
+print('Fim do programa volte sempre')
+
+
