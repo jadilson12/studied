ef439d1a5f5c3c01b0c550a325a2aac7c8b7e538
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:41:06 -0300

    adiciona: calculando pocentagem

---
+++ 12 - Pocentagem.py
@@ -0,0 +1,3 @@
+print('\n -> Loja de modas <- \n -> Gabril Roque <- \n')
+n = float(input('Digite o valor do produto: '))
+print('Que legal você ganhou desconto seu valor ficou por {:.2f}'.format(n - ((n*5)/100)))
