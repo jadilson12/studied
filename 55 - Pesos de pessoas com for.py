3b2d8d88df08ee4cf35a53cb7247113b3502b063
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:13:45 -0300

    adiciona:  " condição for " como calcular os pesos

---
+++ 55 - Pesos de pessoas com for.py
@@ -0,0 +1,17 @@
+maior = 0
+menor = 0
+n_maior = ''
+n_menor = ''
+for c in range(1, 6):
+    n = str(input('Digite {}º nome:'.format(c)))
+    peso = float(input('O {} infome o peso: '.format(n)))
+    if maior < peso:
+        maior = peso
+        n_maior = n
+    else:
+        menor = peso
+        n_menor = n
+print('=+'*10)
+print('''O mais pesado é {} com {}
+O mais leve é {} com {} Kg '''.format(n_maior, maior, n_menor, menor))
+print('=+'*10)
