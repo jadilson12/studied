fc28a5f96e59990310a60b95b9cbf6f19b62729e
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:55:19 -0300

    adiciona:  if e else par ou impar

---
+++ 30 - Se o numero é par ou impar.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+m = (n1 + n2)
+if m%2 == 0:
+    print('A soma dos numeros é par')
+else:
+    print('A soma dos numeros impar')
+print('\nObrigado!')
