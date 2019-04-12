4a2bc2ff58e95a945a6c7dc8dcca78035eea71bf
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:19:21 -0300

    adiciona:  " condição while " como calcular a media com pegunta com varias letras

---
+++ 65 - while  media peguntas de varios numeros.py
@@ -0,0 +1,18 @@
+p='S'
+c=s=m=ep=0
+men = 1
+while p != 'N':
+    v = int(input('Digite uma valor'))
+    c += 1
+    s += v
+    if v > m:
+        m = v
+    else:
+        if  v < m:
+            men = v
+        else:
+            ep += 1
+    med = s / c
+    p = str(input('Deseja continual S/N')).upper()
+print('digitou {} vezes a soma foi {}'.format(c,s))
+print('O maior numero foi {}, o menor {}, foram {} enpates \n a media foi {}'.format(m,men,ep,med))
