ad4388c4e3651f28d0d7afd840316f63cd8e0ba1
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:36:10 -0300

    adiciona: soma 2 valores

---
+++ 04 - soma de 2 valores.py
@@ -0,0 +1,10 @@
+print('*' * 35)
+print('Soma de 2 Valores')
+print('*' * 35)
+n1 = int(input('Favor informe 1ª valor: '))
+n2 = int(input('Favor informe 2ª valor: '))
+total = n1 + n2
+print("*--------------------------------*")
+# print('A soma entre',n1, ' e ',n2 , 'do numero digitado foi ', (n1+n2) )
+print('A soma entre {} e {} vale {}'.format(n1, n2, total))
+print("*--------------------------------*")
