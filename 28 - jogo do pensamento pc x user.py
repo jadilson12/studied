5e038064b1a78fe9b4fd3a7ee5b5585943d7c815
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:54:22 -0300

    adiciona: um pequeno jogo usando if else

---
+++ 28 - jogo do pensamento pc x user.py
@@ -0,0 +1,9 @@
+import random
+
+n = float(input("Pense e digite um numero de 1 a 5 vou tentan adiviar:"))
+pc = random.randrange(0,5)
+if n == pc:
+    print("Legal, Parabens você acertou")
+else:
+    print('Você pendeu \no numero é {}'.format(pc))
+print('---FIM---')
