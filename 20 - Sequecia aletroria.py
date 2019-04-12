8242acda7f7b922252bee97f429dc91e591bfcbd
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:49:02 -0300

    adiciona: sequencias aletroria

---
+++ 20 - Sequecia aletroria.py
@@ -0,0 +1,6 @@
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+ordem = aluno1, aluno2, aluno3, aluno4
+print(sorted(ordem))
