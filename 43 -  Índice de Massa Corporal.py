6b518285c81df3bc6dc51c936ad645ff04b8c64c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:02:25 -0300

    adiciona:  if e else como calcular o indice da massa corporal

---
+++ 43 -  Índice de Massa Corporal.py
@@ -0,0 +1,14 @@
+peso = float(input('Digite o peso:'))
+altura = float(input('Digite altura:'))
+imc = peso /(altura ** 2)
+print('O peso {}Kg com altura de {}mt o IMC foi {:.1f}'.format(peso,altura,imc))
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc < 25:
+    print('Peso Ideial')
+elif imc < 30:
+    print('Sobrepeso')
+elif imc < 40:
+    print('Obesidade')
+else:
+    print('Obesidade morbida')
