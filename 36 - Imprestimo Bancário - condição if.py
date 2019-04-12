dd855e77fba4a1b8cb3d95427439e5ec53748ebc
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:59:00 -0300

    adiciona:  if e else como calcular um imprestimo bancário

---
+++ 36 - Imprestimo Bancário - condição if.py
@@ -0,0 +1,14 @@
+#escreva um programa para aprovar o emprestimo bancario para comprar uma casa.
+casa = float(input("Valor da casa: R$"))
+salario = float(input("Salário do comprador : R$"))
+anos = int(input("Quantos anos de finaciamento: "))
+pestacao = casa / (anos * 12)
+minimo = (salario*30) /100
+print('Para pagar a casa de {:.2f} em {} anos'.format(casa,anos), end='')
+print(' o valor da pestação é R${:.2f}'.format(pestacao))
+
+if minimo >= pestacao:
+    print("\n{}\n".format('Parabéns! Seu empréstimo foi Aprovado'))
+else:
+    print("\n{}\n ".format('Desculpe seu cretido não foi aprovado'))
+
