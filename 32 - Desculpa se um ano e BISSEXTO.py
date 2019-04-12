d4f7a6445bad33f7ff1234112970ea2fc87dd80d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:56:09 -0300

    adiciona:  if e else calculando anos bissexto

---
+++ 32 - Desculpa se um ano e BISSEXTO.py
@@ -0,0 +1,6 @@
+ano = int(input('Digite data qualquer ex: 1900:'))
+if (ano % 4 == 0) and (ano % 100 != 0) :
+    print('Este ano {} é bissexto'.format(ano))
+else:
+    print('O ano que você digitou não é bissexto.')
+print('---FIM----')
