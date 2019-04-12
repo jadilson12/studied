434630d09ea5fa17048eb9a982fa7aa7b7bc5f35
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:40:51 -0300

    adiciona: dimessão altura e largura

---
+++ 11 - Dimessão altura e largura.py
@@ -0,0 +1,4 @@
+altura = float(input("Digite a altura: "))
+largura = float(input('Digite a largura: '))
+area = altura * largura
+print('A area é {} Metros, então vai precisar de {} latas de tintas'.format(area,area / 2))
