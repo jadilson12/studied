383bac44ba36b07dfa0c1b4a96cc3928b4f721ad
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:08:18 -0300

    adiciona:  " condição for " termos da razão de PA

---
+++ 51 - Termos da razão de PA.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Digite o 1ª termo de uma PA: '))
+razao = int(input('Digite a razão dessa PA: '))
+decimo =primeiro +(10 - 1 ) * razao
+for c in range(primeiro, decimo + razao ,razao):
+    print('{}'.format(c), end=' ► ')
+print('Acabou')
