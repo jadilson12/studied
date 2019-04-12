8fd13e6d415bb14de8d3d8735cafa54309567027
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:35:28 -0300

    adiciona: escrevendo na tela

---
+++ 01 - escrever na tela.py
@@ -0,0 +1,10 @@
+print('*'*40)
+print("\033[7;31m---------Nome - Idade - Peso -----------\033[m")
+print('*'*40)
+nome = input("Favor digite seu nome:  ")
+idade = int(input("Favor digite sua Idade:  "))
+peso = input("Favor digite seu peso: ")
+
+print('olá {}, sejam bem vindos, sua idade é {}, anos e você tem {} quilos'.format(nome,idade,peso))
+
+
