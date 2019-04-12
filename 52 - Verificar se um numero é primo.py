2310f13f2bc920d5cf61d9ca6df0821c8e88881d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 13:09:06 -0300

    adiciona:  verificar se o numero e par ou impar II

---
+++ 52 - Verificar se um numero é primo.py
@@ -0,0 +1,16 @@
+n = int(input('Digite um valor'))
+cont = 0
+print('O numero informado é divisível por: ')
+for c in range(1,n+1):
+    if n%c == 0:
+        print('\033[31m', end=' ')
+        cont += 1
+    else:
+        print('\033[38m', end = ' ')
+    print('{}'.format(c), end = ' ')
+
+print('\n\033[mO numero {} foi divisivel por {} vezes'.format(n,cont))
+if cont <= 2:
+    print('É por isso que ele é primo')
+else:
+    print('É por isso que ele NÂO é primo')
