1eff9e05fbc6b7a635f62ff53f01b3ac124b5880
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:39:04 -0300

    adiciona: media do alunos

---
+++ 07 - Media do aluno.py
@@ -0,0 +1,5 @@
+aluno = input('Digite o nome do aluno: ')
+nt1 = float(input('Digite primeira nota: '))
+nt2 = float(input('Digite segunda nota: '))
+m = ((nt1 + nt2) / 2)
+print('O {} obteve a média de {}'.format(aluno, m))
