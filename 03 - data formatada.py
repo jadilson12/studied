1a1cb3841a51b20ea0818021884180f886968d2d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 12:35:45 -0300

    adiciona: formatando data

---
+++ 03 - data formatada.py
@@ -0,0 +1,9 @@
+print("*--------------------------------*")
+print("------Data formatada     --------")
+print("*--------------------------------*")
+dia = input("Favor informe um dia: ")
+mes = input('Favor informe o mês: ')
+ano = input('Favor informe o ano: ')
+print("*--------------------------------*")
+print('Você nasceu em ',dia,' de ',mes,' de ',ano,': correto ?')
+
