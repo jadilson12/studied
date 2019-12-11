421d742bb0eef2e94ea702a843a60a7df303169d
Jadilson Guedes <jadilson12@gmail.com>
2019-12-11 09:32:56 -0300

    removendo o que quero

--- cursos/algaWorks/app-api/src/main/java/com/worksplus/app/repository/OportunidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.worksplus.app.repository;
-
-import com.worksplus.app.model.Oportunidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
-    Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
-}
